5813297374335632460ea3c179db117707867253
Christo du Toit <christo@dutoit.co.uk>
2026-06-13 02:22:10 +0100

    CODE RUB: Updated Migrations

--- StudentApp.Core/Migrations/20260613011656_PendingModelChanges.Designer.cs
+++ StudentApp.Core/Migrations/20260613011656_PendingModelChanges.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using StudentApp.Core.Brokers.Storages;
 
@@ -11,9 +12,11 @@ using StudentApp.Core.Brokers.Storages;
 namespace StudentApp.Core.Migrations
 {
     [DbContext(typeof(StorageBroker))]
-    partial class StorageBrokerModelSnapshot : ModelSnapshot
+    [Migration("20260613011656_PendingModelChanges")]
+    partial class PendingModelChanges
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ StudentApp.Core/Migrations/20260613011656_PendingModelChanges.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StudentApp.Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class PendingModelChanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedBy",
+                table: "Students",
+                type: "nvarchar(255)",
+                maxLength: 255,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "CreatedWhen",
+                table: "Students",
+                type: "datetimeoffset",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.AddColumn<string>(
+                name: "DeletedBy",
+                table: "Students",
+                type: "nvarchar(255)",
+                maxLength: 255,
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "DeletedWhen",
+                table: "Students",
+                type: "datetimeoffset",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "DeletionReason",
+                table: "Students",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Students",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UpdatedBy",
+                table: "Students",
+                type: "nvarchar(255)",
+                maxLength: 255,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "UpdatedWhen",
+                table: "Students",
+                type: "datetimeoffset",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedBy",
+                table: "Enrollments",
+                type: "nvarchar(255)",
+                maxLength: 255,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "CreatedWhen",
+                table: "Enrollments",
+                type: "datetimeoffset",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.AddColumn<string>(
+                name: "DeletedBy",
+                table: "Enrollments",
+                type: "nvarchar(255)",
+                maxLength: 255,
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "DeletedWhen",
+                table: "Enrollments",
+                type: "datetimeoffset",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "DeletionReason",
+                table: "Enrollments",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Enrollments",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UpdatedBy",
+                table: "Enrollments",
+                type: "nvarchar(255)",
+                maxLength: 255,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "UpdatedWhen",
+                table: "Enrollments",
+                type: "datetimeoffset",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.CreateTable(
+                name: "ProcessedEvents",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    EventId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ReceiverName = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    ProcessedAt = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProcessedEvents", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProcessedEvents_EventId_ReceiverName",
+                table: "ProcessedEvents",
+                columns: new[] { "EventId", "ReceiverName" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProcessedEvents");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedWhen",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedBy",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedWhen",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "DeletionReason",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedBy",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedWhen",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "Enrollments");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedWhen",
+                table: "Enrollments");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedBy",
+                table: "Enrollments");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedWhen",
+                table: "Enrollments");
+
+            migrationBuilder.DropColumn(
+                name: "DeletionReason",
+                table: "Enrollments");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Enrollments");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedBy",
+                table: "Enrollments");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedWhen",
+                table: "Enrollments");
+        }
+    }
+}
